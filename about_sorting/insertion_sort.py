c8fa7f6f964e91a7298333af603124af31a487ed
VersionBeathon <s875766683@hotmail.com>
2017-01-23 16:43:46 +0800

    try/except

--- about_sorting/insertion_sort.py
+++ about_sorting/insertion_sort.py
@@ -6,6 +6,7 @@
 
 
 def insert_sort(array, **kwargs):
+
     if not kwargs:
 
         length = len(array)
@@ -20,22 +21,25 @@ def insert_sort(array, **kwargs):
                     array[j + 1] = array[j]
                     j -= 1
                 array[j + 1] = temp
-    if kwargs['reverse']:
+    try:
+        if kwargs['reverse']:
 
-        length = len(array)
+            length = len(array)
 
-        for i in range(1, length):
-            j = i - 1
-            if array[i] > array[j]:
-                temp = array[i]
-                array[i] = array[j]
-                j -= 1
-                while j >= 0 and array[j] < temp:
-                    array[j + 1] = array[j]
+            for i in range(1, length):
+                j = i - 1
+                if array[i] > array[j]:
+                    temp = array[i]
+                    array[i] = array[j]
                     j -= 1
-                array[j + 1] = temp
+                    while j >= 0 and array[j] < temp:
+                        array[j + 1] = array[j]
+                        j -= 1
+                    array[j + 1] = temp
+    except:
+        pass
     return array
 
 if __name__ == "__main__":
     _list = [49, 38, 65, 97, 76, 13, 27, 49]
-    print(insert_sort(_list, reverse=True))
+    print(insert_sort(_list))
