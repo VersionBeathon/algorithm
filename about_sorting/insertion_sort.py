21f4e74e29789baee974203e968d526028d26355
VersionBeathon <s875766683@hotmail.com>
2017-01-23 14:32:09 +0800

    Init

---
+++ about_sorting/insertion_sort.py
@@ -0,0 +1,26 @@
+#!/user/bin/env python
+# _*_coding:utf-8_*_
+# @Time   :2017/1/23 14:05
+# @Author :Kira
+# @Software：PyCharm
+
+
+def insert_sort(array):
+
+    length = len(array)
+
+    for i in range(1, length):
+        j = i - 1
+        if array[i] < array[j]:
+            temp = array[i]
+            array[i] = array[j]
+            j -= 1
+            while j >= 0 and array[j] > temp:
+                array[j + 1] = array[j]
+                j -= 1
+            array[j + 1] = temp
+    return array
+
+
+_list = [49, 38, 65, 97, 76, 13, 27, 49]
+print(insert_sort(_list))
